c44579eca734d6b4ce87115250c6e65faa33c0db
superrm11 <ramtech51@comcast.net>
2019-02-14 21:47:09 -0500

    Fixed it so now it compiles :)

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+enum AutoPath
+{
+
+};
+
+
 /**
  * Runs the user autonomous code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -12,5 +18,5 @@
  * from where it left off.
  */
 void autonomous() {
-
+  
 }
